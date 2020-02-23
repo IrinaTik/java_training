7512de208f02d205f07353504c09a18e3d52338c
IrinaTik <60540077+IrinaTik@users.noreply.github.com>
2020-02-23 14:58:57 +0300

    Добавлены проверки вводимого текста для тестов создания и модификации групп

--- addressbook_webtests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook_webtests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -18,9 +18,15 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
-    wb.findElement(locator).clear();
-    wb.findElement(locator).sendKeys(text);
+    if (text != null) {
+      String existingText = wb.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
+        wb.findElement(locator).clear();
+        wb.findElement(locator).sendKeys(text);
+      }
+    }
   }
+
   public boolean isAlertPresent() {
     try {
       wb.switchTo().alert();
