1a983f3f32322caaffe1a9b1f97f18e2713e5077
IrinaTik <60540077+IrinaTik@users.noreply.github.com>
2020-03-13 20:19:27 +0300

    Задание №11: сведение тестов адресов, телефонов и почты в единый тест

--- addressbook_webtests/src/test/java/ru/stqa/pft/addressbook/tests/ContactConnectDataTests.java
+++ addressbook_webtests/src/test/java/ru/stqa/pft/addressbook/tests/ContactConnectDataTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.datamodel.ContactConnectDATA;
 import ru.stqa.pft.addressbook.datamodel.ContactPersonalDATA;
@@ -11,30 +10,36 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase{
+public class ContactConnectDataTests extends TestBase {
 
   @Test
-  public void testContactPhones() {
+  public void testContactConnectData () {
     app.goTo().homepage();
     ContactPersonalDATA contact = app.contact().all().iterator().next();
     ContactPersonalDATA contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-  /*  assertThat(contact.getContactConnectDATA().getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getContactConnectDATA().getHomePhone())));
-    assertThat(contact.getContactConnectDATA().getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getContactConnectDATA().getMobilePhone())));
-    assertThat(contact.getContactConnectDATA().getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getContactConnectDATA().getWorkPhone()))); */
-
     assertThat(contact.getContactConnectDATA().getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm.getContactConnectDATA())));
+    assertThat(contact.getContactConnectDATA().getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm.getContactConnectDATA())));
+    assertThat(contact.getContactConnectDATA().getAddress(), equalTo(contactInfoFromEditForm.getContactConnectDATA().getAddress()));
   }
 
   private String mergePhones(ContactConnectDATA contactPhones) {
     return Arrays.asList(contactPhones.getHomePhone(), contactPhones.getMobilePhone(), contactPhones.getWorkPhone())
             .stream().filter((s) -> ! s.equals(""))
-            .map(ContactPhoneTests::cleaned) //применить к элементам потока функцию
+            .map(ContactConnectDataTests::cleaned) //применить к элементам потока функцию
             .collect(Collectors.joining("\n"));
     //отбрасывает пустые строки, чистит от лишних символов, вставляет переход строки
+  }
+
+  private String mergeEmails(ContactConnectDATA contactEmails) {
+    return Arrays.asList(contactEmails.getEmail_1(), contactEmails.getEmail_2(), contactEmails.getEmail_3())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
   }
 
   public static String cleaned (String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
+
+
 }
