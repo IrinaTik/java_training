548b99639c158b8d030a821beae781be21597757
IrinaTik <60540077+IrinaTik@users.noreply.github.com>
2020-04-06 16:31:37 +0300

    Лекция 8.5. FTP передача файлов

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_signup_use_captcha = OFF;
+
+$g_default_timezone = 'Europe/Berlin';
+$g_crypto_master_salt = 'Flj1eB4Q7WHVhxs3wD1Xiq0nLf+DbK0zI3HJQXVbia0=';
