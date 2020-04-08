6a2a0e6c7c112769fe699f8335719ea72c8075af
IrinaTik <60540077+IrinaTik@users.noreply.github.com>
2020-04-08 13:52:34 +0300

    Fix for james

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -14,7 +14,7 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
+ // @BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -22,13 +22,13 @@ public class RegistrationTests extends TestBase {
   @Test
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
     String user = String.format("user%s", now);
     String password = "password";
-//    app.james().createUser(user, password); //создание юзера на внещнем почтовом сервере
+    app.james().createUser(user, password); //создание юзера на внещнем почтовом сервере
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000); //внутренний почтовый сервер
-   // List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000); //внешний почтовый сервер
+  //  List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000); //внутренний почтовый сервер
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000); //внешний почтовый сервер
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -36,7 +36,7 @@ public class RegistrationTests extends TestBase {
 
 
 
-  @AfterMethod(alwaysRun = true)
+//  @AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
